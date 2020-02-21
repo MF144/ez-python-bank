ee6d38ca73734375fa157cf0c1e435279366db09
MF144 <43513381+MF144@users.noreply.github.com>
2020-02-21 21:03:07 +0800

    Add files via upload
    Bank code

---
+++ ezbankpy.py
@@ -0,0 +1,48 @@
+class Account:
+    def __init__(self, balance, deposit, withdraw):
+        self.balance = balance
+        self.deposit = deposit
+        self.withdraw = withdraw
+
+    def dp(self):
+        self.deposit = float(input("How much would you like to deposit? "))
+        self.balance += self.deposit
+        print("P%s" % (self.deposit) + " has been added to your account.")
+
+    def wd(self):
+        self.withdraw = float(input("How much would you like to withdraw? "))
+        self.balance -= self.withdraw
+        print("P%s" % (self.withdraw) +
+              " has been subtracted from your account.")
+
+    def bal(self):
+        print("Your current balance is P" + "%s" % (self.balance))
+
+    def Menu(self):
+        print("Welcome to Bank of Batangas!")
+        print("[1] Deposit")
+        print("[2] Withdrawal")
+        print("[3] Balance")
+        print("[4] Exit")
+
+
+acc = Account(2000, 0, 0)
+
+while True:
+
+    print("Welcome to Bank of Batangas!")
+    print("[1] Deposit")
+    print("[2] Withdrawal")
+    print("[3] Balance")
+    print("[4] Exit")
+
+    n = int(input("Enter your choice: "))
+
+    if n == 1:
+        acc.dp()
+    elif n == 2:
+        acc.wd()
+    elif n == 3:
+        acc.bal()
+    elif n == 4:
+        break
